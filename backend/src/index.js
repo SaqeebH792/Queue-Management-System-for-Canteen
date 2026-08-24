c36fcf92bc9189a466734c742f31dacc77494750
SaqeebH792 <saqeebhassan792@gmail.com>
2026-08-24 07:00:43 -0700

    Database Connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,32 +7,32 @@ import { app } from "./app.js";
 import { connectDB } from "./db/index.js";
 
 import http from "http";
-import { Server } from "socket.io";
-import { setSocketIO } from "./socket.js";
+// import { Server } from "socket.io";
+// // import { setSocketIO } from "./socket.js";
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  },
-});
+// const io = new Server(server, {
+//   cors: {
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true,
+//   },
+// });
 
-setSocketIO(io);
+// setSocketIO(io);
 
-io.on("connection", socket => {
-  console.log("User Connected:", socket.id);
+// io.on("connection", socket => {
+//   console.log("User Connected:", socket.id);
 
-  socket.on("join", userId => {
-    socket.join(userId);
-    console.log(`User ${userId} joined room`);
-  });
+//   socket.on("join", userId => {
+//     socket.join(userId);
+//     console.log(`User ${userId} joined room`);
+//   });
 
-  socket.on("disconnect", () => {
-    console.log("User Disconnected:", socket.id);
-  });
-});
+//   socket.on("disconnect", () => {
+//     console.log("User Disconnected:", socket.id);
+//   });
+// });
 
 const PORT = process.env.PORT || 8080;
 
